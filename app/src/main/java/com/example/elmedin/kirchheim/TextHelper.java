60994aff0362e34320f085736e7c16f46780b5b3
Mark Benson <benson@hm.edu>
2016-02-02 12:14:09 +0100

    Change Impressum appearance

--- app/src/main/java/com/example/elmedin/kirchheim/TextHelper.java
+++ app/src/main/java/com/example/elmedin/kirchheim/TextHelper.java
@@ -1,14 +1,24 @@
+package com.example.elmedin.kirchheim;
+
+import android.graphics.Typeface;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+
 public class TextHelper{
 
-    public SpannableString formatText(int lvl, String content){
+    public static SpannableString formatText(int lvl, String content){
 
-        int tmpLenght = content.lenght -1;
+        int tmpLenght = content.length() -1;
         SpannableString output = new SpannableString(content);
 
                 switch(lvl){
                     case 1: output.setSpan(new StyleSpan(Typeface.BOLD), 0 , tmpLenght, 0);
                         break;
-                    case 2: output.setSpan(new StyleSpan(Typeface.BOLD), 0 , tmpLenght, 0);
+                    case 2:
+                        output.setSpan(new StyleSpan(Typeface.BOLD_ITALIC), 0 , tmpLenght, 0);
+                        break;
+                    case 3:  output.setSpan(new RelativeSizeSpan(0.75f), 0,tmpLenght,0);
                         break;
                     default:
                         break;
