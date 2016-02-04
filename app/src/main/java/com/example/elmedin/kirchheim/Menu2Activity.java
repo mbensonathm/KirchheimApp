8b5d246e90dd1e7c1e4e670822ca2a5a5761f2f8
Mark Benson <benson@hm.edu>
2016-02-04 16:19:26 +0100

    Menu2

--- app/src/main/java/com/example/elmedin/kirchheim/Menu2Activity.java
+++ app/src/main/java/com/example/elmedin/kirchheim/Menu2Activity.java
@@ -2,6 +2,7 @@ package com.example.elmedin.kirchheim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Menu2Activity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Menu2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
--- app/src/main/java/com/example/elmedin/kirchheim/MenuActivity.java
+++ app/src/main/java/com/example/elmedin/kirchheim/MenuActivity.java
@@ -1,7 +1,48 @@
 package com.example.elmedin.kirchheim;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
 /**
  * Created by Mark on 03-Feb-16.
  */
-public class MenuActivity {
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_list_view);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
+//        setSupportActionBar(toolbar);
+
+        final ListView listview = (ListView) findViewById(R.id.menuList);
+        String[] values = new String[]{"Restaurants & Bars", "Hotels & Pensionen", "Ferienhäuser & -wohnungen",
+                "Shopping", "Gesundheit", "Bildungseinrichtungen", "Lifestyle & Aktivitäten", "Recht & Finanzen",
+                "Sehenswürdigkeiten", "Unternehmen"};
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+    }
 }
